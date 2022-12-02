e77d7dfc230aeb6fa89c4d52ce1fcb803b698cd7
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-02 17:12:54 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+int x1 = x / 10 % 10; // решение
+
+
+// Поиск остальных чисел 
+int x2 = x / 100;
+int x3 = x % 10;
+Console.WriteLine(x1);
