c6bc2f3caf8c85add15c2697c9ac0ec94189fd1e
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-02 18:52:07 +0300

    UPD Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine ("Введите число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+ 
+while (n > 999)
+ {
+    n /= 10;
+ }
+
+if (n > 99)
+{ 
+    int x = ((n % 100)% 10);
+    
+    Console.WriteLine (x);
+}
+else
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
