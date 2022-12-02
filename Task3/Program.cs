a2246a07b9791fa1966b1e780c49365f0be7e531
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-02 19:19:41 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine ("Введите день недели от 1 до 7: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ( n >= 1 && n <= 5)
+{
+    Console.WriteLine ("Будний день");
+}
+else if (n == 6 | n == 7)
+{
+Console.WriteLine ("Выходной");
+}
+else if ( n > 7 || n < 1)
+{
+Console.WriteLine ("Такого дня не существует");
+}
