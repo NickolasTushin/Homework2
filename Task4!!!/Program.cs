dcf5895194daa7433c188e772d9f719fa8317e4d
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-02 21:18:03 +0300

    add Task4

---
+++ Task4!!!/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите числа");
+int a = Convert.ToInt32(Console. ReadLine ());
+int max = a;
+int max2 = a;
+while (a != 0)
+{
+a = Convert. ToInt32(Console. ReadLine ());
+if (a > max ) 
+{
+max2 = max;
+max = a;
+}
+}
+Console.WriteLine ($"end");
+
+Console.WriteLine($"Второе максимальное число {max2}");
