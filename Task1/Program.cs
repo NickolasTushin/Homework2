d8266e714617229af5a59be5058aa30d79f540c8
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-02 17:33:45 +0300

    UPD Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,13 +1,15 @@
 ﻿Console.Clear();
 
 Console.WriteLine("Введите трехзначное число: ");
-
 int x = Convert.ToInt32(Console.ReadLine());
-
-int x1 = x / 10 % 10; // решение
-
-
-// Поиск остальных чисел 
-int x2 = x / 100;
-int x3 = x % 10;
-Console.WriteLine(x1);
+if (x >= 100 && x <= 999)
+    { 
+    int x2 = x / 100;
+    int x3 = x % 10;
+    int x1 = x / 10 % 10; // решение
+    Console.WriteLine(x1);
+    } 
+else  
+    {
+        Console.WriteLine ("Число не трехзначное");
+    }
